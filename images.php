ba1fe885bdaf39f14dbbcc4c94b9709686fd77c7
Zubs <zubairidrisaweda@gmail.com>
2020-11-10 15:36:20 +0100

    Done with view images page

--- images.php
+++ images.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Uploaded Images</title>
+
+	<!-- Just to add some style -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body class="container py-5">
@@ -11,14 +13,39 @@
 		<h1>Here's all our uploaded images</h1>
 		<small>You can add new images <a href="index.html">here</a></small>
 		<div class="row">
-			<div class="col-md-6 px-5 py-5">
-				<div class="card">
-					<div class="card-body">
-						<img src="#" alt="Image will go here" width="100%">
+
+			<!-- Let's handle logic -->
+			<?php
+
+				// Connect to database
+				$conn = mysqli_connect('localhost', 'root', '', 'file_upload') or die("Unable to connect to db");
+
+				// Fetch images from database
+				$query = "SELECT * FROM images";
+
+				// Run the query to actually get the data
+				$data = mysqli_query($conn, $query) or die("Unable to fetch");
+
+				// Display images, by looping over them
+				while ($row = mysqli_fetch_array($data)) {
+			?>
+
+				<!-- I decided to write normal HTML instead of using too many echos -->
+				<div class="col-md-6 px-5 py-5">
+					<div class="card">
+						<div class="card-body">
+							<img src="#" alt="Image will go here" width="100%">
+						</div>
+						<div class="card-title">Tets</div>
 					</div>
-					<div class="card-title">Tets</div>
 				</div>
-			</div>
+			<?php
+				}
+
+				// Close connection
+				mysqli_close($conn);
+
+			?>
 		</div>
 	</div>
 </body>
--- upload.php
+++ upload.php
@@ -1,16 +1,22 @@
 <?php
 
-// Get form inputs
-$name = $_POST['name'];
-
 // Just to be sure the form was submitted
 if (isset($_POST['submit'])) {
+
+	// Get form inputs
+	$name = $_POST['name'];
 
 	// Just some additional validation, not like we really need the name
 	if (!empty($name)) {
 
-		// Connect To Database
-		$connection = mysql_connect() or die("Unable to connect to db")
+		// Connect To Database 
+		$connection = mysqli_connect('localhost', 'root', '', 'file_upload') or die("Unable to connect to db");
+
+		// Write the MySQL query here
+		$query = "INSERT INTO images (name, image, date_) VALUES ()";
+
+		// Run The Query
+		$result = mysqli_query($query) or die("Unable To Add New Row");
 	} else {
 		die();
 	}
